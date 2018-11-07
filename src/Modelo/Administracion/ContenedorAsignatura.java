05b0c79cecc3b3f1c73567512b073e417621526f
Thony <anthonymmarinbolivar@gmail.com>
2018-11-07 01:12:42 -0600

    Devolviendo a una version anterior

---
+++ src/Modelo/Administracion/ContenedorAsignatura.java
@@ -0,0 +1,28 @@
+
+package Modelo.Administracion;
+
+import java.util.ArrayList;
+
+public class ContenedorAsignatura implements IEntidadAsignatura{
+
+    @Override
+    public boolean agregarAsignatura(EntidadAsignatura asignatura) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean modificarAsignatura(EntidadAsignatura asignatura) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean eliminarAsignatura(String codigo) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<EntidadAsignatura> listar() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
