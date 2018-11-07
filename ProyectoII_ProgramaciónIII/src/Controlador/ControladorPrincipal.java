d0448201fd2c6afe28d94e02735e74b7a6c5a693
Thony <anthonymmarinbolivar@gmail.com>
2018-11-07 00:52:22 -0600

    Revert "Algunos avances más"
    This reverts commit c11e57d7523489242bce441cee77ad71ef558449.

---
+++ ProyectoII_ProgramaciónIII/src/Controlador/ControladorPrincipal.java
@@ -0,0 +1,14 @@
+
+package Controlador;
+
+import Vista.InterfazPrincipal;
+
+public class ControladorPrincipal {
+    
+private InterfazPrincipal interfazPrincipal;    
+    
+    public ControladorPrincipal(){
+        interfazPrincipal = new InterfazPrincipal();
+    }
+    
+}
