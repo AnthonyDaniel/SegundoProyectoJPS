e9ac865504ad58b88f9b9e11c1db4058899489c6
AnthonyDaniel <39199700+AnthonyDaniel@users.noreply.github.com>
2018-11-12 14:51:26 -0600

    Update 3.0

--- src/Controlador/ControladorLogin.java
+++ src/Controlador/ControladorLogin.java
@@ -3,6 +3,8 @@ package Controlador;
 
 import Vista.Interfaz;
 import Vista.Login;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ControladorLogin {
     
@@ -11,12 +13,19 @@ public class ControladorLogin {
     public ControladorLogin(Interfaz e, Login a){
     
         interfaz = e;
+        BotonAceptar(e,a);
         
     }
     
     private void BotonAceptar(Interfaz e, Login a){
 
-       
+       a.jButtonAceptar.addActionListener(new ActionListener() {
+           @Override
+           public void actionPerformed(ActionEvent ae) {
+              a.dispose();
+              e.setVisible(true);
+           }
+       });
         
         
     }
