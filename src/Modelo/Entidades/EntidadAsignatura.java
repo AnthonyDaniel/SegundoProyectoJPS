351099be97db2e9a0fe566f87421ba0582fb6fa1
KelinSilva <kasvillarrealgmail.com>
2018-11-12 09:17:15 -0600

    EntidadAsignatura
    Entidad sin relaciones

--- src/Modelo/Entidades/EntidadAsignatura.java
+++ src/Modelo/Entidades/EntidadAsignatura.java
@@ -5,9 +5,36 @@ import java.io.Serializable;
 
 public class EntidadAsignatura implements Serializable{
 
+    private static final long serialVersionID=1L;
+    
+    private int codigoAsignatura;
+    private String nombreAsignatura;
+   
+    private EntidadProfesor profesor;
+   
     public EntidadAsignatura(){
     
     
     }
     
+    public int getCodigoAsignatura() {
+        return codigoAsignatura;
+    }
+    public void setCodigoAsignatura(int codigoAsignatura) {
+        this.codigoAsignatura = codigoAsignatura;
+    }
+    
+    public String getNombreAsignatura() {
+        return nombreAsignatura;
+    }
+    public void setNombreAsignatura(String nombreAsignatura) {
+        this.nombreAsignatura = nombreAsignatura;
+    }
+    
+    public EntidadProfesor getProfesor() {
+        return profesor;
+    }
+    public void setProfesor(EntidadProfesor profesor) {
+        this.profesor = profesor;
+    }
 }
