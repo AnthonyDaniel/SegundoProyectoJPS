2169e91f139a9789d3a21b116594285bc4139fb7
AnthonyDaniel <39199700+AnthonyDaniel@users.noreply.github.com>
2018-11-12 09:29:52 -0600

    Update

--- src/Modelo/Entidades/EntidadAusencia.java
+++ src/Modelo/Entidades/EntidadAusencia.java
@@ -5,4 +5,28 @@ import java.io.Serializable;
 
 public class EntidadAusencia implements Serializable{
     
+    private String fecha;
+    private EntidadEstudiante estudiante;
+    
+    public EntidadAusencia (){
+    
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public EntidadEstudiante getEstudiante() {
+        return estudiante;
+    }
+
+    public void setEstudiante(EntidadEstudiante estudiante) {
+        this.estudiante = estudiante;
+    }
+    
+    
 }
--- src/Modelo/Entidades/EntidadEstudiante.java
+++ src/Modelo/Entidades/EntidadEstudiante.java
@@ -5,8 +5,83 @@ import java.io.Serializable;
 
 public class EntidadEstudiante implements Serializable {
     
+    private String cedula;
+    private String nombre;
+    private String apellidos;
+    private String correo;
+    private String telefono;
+    private int edad;
+    
+    private EntidadAusencia ausencia;
+    
+    private EntidadAsignatura asignatura;
+    
     public EntidadEstudiante(){
     
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public EntidadAusencia getAusencia() {
+        return ausencia;
+    }
+
+    public void setAusencia(EntidadAusencia ausencia) {
+        this.ausencia = ausencia;
+    }
+
+    public EntidadAsignatura getAsignatura() {
+        return asignatura;
+    }
+
+    public void setAsignatura(EntidadAsignatura asignatura) {
+        this.asignatura = asignatura;
     }
     
 }
