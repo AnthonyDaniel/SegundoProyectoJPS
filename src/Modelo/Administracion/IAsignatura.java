18c697b85db177036ebf672a22496f0884bf9511
mauVR <44210264+mauVR@users.noreply.github.com>
2018-11-13 19:26:23 -0600

    IProfesor
    añade interface, contenedor y controlador a profesor

--- src/Modelo/Administracion/IAsignatura.java
+++ src/Modelo/Administracion/IAsignatura.java
@@ -5,7 +5,7 @@ import Modelo.Entidades.EntidadAsignatura;
 import java.util.ArrayList;
 
 public interface IAsignatura {
-     public boolean agregarAsignatura(EntidadAsignatura asignatura) throws Exception;
+    public boolean agregarAsignatura(EntidadAsignatura asignatura) throws Exception;
     public boolean modificarAsignatura(EntidadAsignatura asignatura) throws Exception;
     public boolean eliminarAsignatura(String codigo) throws Exception;
     public ArrayList<EntidadAsignatura>listar() throws Exception; //este metodo quizas sea map
---
+++ src/Modelo/Administracion/IProfesor.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo.Administracion;
+
+import Modelo.Entidades.EntidadEstudiante;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Mauricio
+ */
+public interface IProfesor {
+    public ArrayList<EntidadEstudiante> listarEstudiantes
+}
