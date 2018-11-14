2b393d5b45a1d6fd554a137e5e8a4d1eb6a45e03
KelinSilva <kasvillarrealgmail.com>
2018-11-14 00:00:10 -0600

    update
    asignaturaMetodos

--- src/Modelo/Administracion/ContenedorAsignatura.java
+++ src/Modelo/Administracion/ContenedorAsignatura.java
@@ -2,13 +2,37 @@
 package Modelo.Administracion;
 
 import Modelo.Entidades.EntidadAsignatura;
+import Modelo.Hibernate.HibernateUtil;
 import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+import org.hibernate.SQLQuery;
+import org.hibernate.Session;
 
 public class ContenedorAsignatura implements IAsignatura{
 
     @Override
     public boolean agregarAsignatura(EntidadAsignatura asignatura) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+      
+      Session session = new HibernateUtil().buildSessionFactory().openSession();        
+      session.beginTransaction();
+      boolean exito;
+        
+      try{ 
+      //insertar una entidad
+      session.save(asignatura);
+      
+      session.getTransaction().commit();
+      session.close();
+      
+      exito=true;
+       }catch(Exception ex){
+             exito=false;
+             JOptionPane.showMessageDialog(null, "No se pudo cargar el archivo", 
+             "WARNING", JOptionPane.WARNING_MESSAGE);
+       }
+      
+         return exito;
     }
 
     @Override
@@ -22,8 +46,19 @@ public class ContenedorAsignatura implements IAsignatura{
     }
 
     @Override
-    public ArrayList<EntidadAsignatura> listar() throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public List<EntidadAsignatura> listar(String nombreAsignatura) throws Exception {
+        
+        Session session = new HibernateUtil().buildSessionFactory().openSession();        
+        session.beginTransaction();
+
+        SQLQuery consulta =  session.createSQLQuery("SELECT * FROM colegiomav WHERE asignatura= "+nombreAsignatura);
+
+        List datos = consulta.list();
+        
+        session.getTransaction().commit();
+        session.close();
+        
+        return datos;
     }
     
 }
--- src/Modelo/Administracion/IAsignatura.java
+++ src/Modelo/Administracion/IAsignatura.java
@@ -3,10 +3,11 @@ package Modelo.Administracion;
 
 import Modelo.Entidades.EntidadAsignatura;
 import java.util.ArrayList;
+import java.util.List;
 
 public interface IAsignatura {
     public boolean agregarAsignatura(EntidadAsignatura asignatura) throws Exception;
     public boolean modificarAsignatura(EntidadAsignatura asignatura) throws Exception;
     public boolean eliminarAsignatura(String codigo) throws Exception;
-    public ArrayList<EntidadAsignatura>listar() throws Exception; //este metodo quizas sea map
+    public List<EntidadAsignatura>listar(String nombreAsignatura) throws Exception; //este metodo quizas sea map
 }
